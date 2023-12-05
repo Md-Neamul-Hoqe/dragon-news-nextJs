8e816327187dd8cbf338517422edf4f04cabf533
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-12-05 19:12:20 +0600

    Dragon news v2 init with nextjs and material ui, Shared component app bar

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,84 @@
+'use client';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Image from 'next/image';
+import logo from '@/assets/logo.svg';
+import { IconButton, Stack } from '@mui/material';
+
+// icons 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Link from 'next/link';
+
+
+const pages = [ 'Products', 'Pricing', 'Blog' ];
+
+const navItems = [
+    {
+        route: 'Home',
+        pathname: '/'
+    },
+    {
+        route: 'Pages',
+        pathname: '/pages'
+    },
+    {
+        route: 'Category',
+        pathname: '/category'
+    },
+    {
+        route: 'News',
+        pathname: '/news'
+    },
+    {
+        route: 'About',
+        pathname: '/about'
+    },
+    {
+        route: 'Contact',
+        pathname: '/contact'
+    },
+]
+
+const Navbar = () => {
+
+    return (
+        <AppBar position="static" className='bg-black'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Image src={logo} width={100} height={100} alt='logo' />
+                    <Box className="w-full text-center">
+                        {navItems.map((item) => (
+                            <Link
+                                key={item}
+                                href={item?.pathname}
+                            >
+                                <Button className="text-white">{item?.route}</Button>
+                            </Link>
+                        ))}
+                    </Box>
+                    <Box>
+                        <Stack direction="row" sx={{ "& svg": { color: 'white' } }}>
+                            <IconButton aria-label="delete">
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton aria-label="delete">
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton aria-label="delete">
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton aria-label="delete">
+                                <FacebookIcon />
+                            </IconButton>
+                        </Stack>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar >
+    );
+}
+export default Navbar;
